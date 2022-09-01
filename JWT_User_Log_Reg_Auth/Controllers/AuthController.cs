9eac484e8c33f7cabeccb73e04e471ca472a069b
arsen211101 <arsen.belluyan@outlook.com>
2022-09-01 15:40:57 +0400

    created PasswordHash() method

--- JWT_User_Log_Reg_Auth/Controllers/AuthController.cs
+++ JWT_User_Log_Reg_Auth/Controllers/AuthController.cs
@@ -1,6 +1,7 @@
 ﻿using JWT_User_Log_Reg_Auth.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
 
 namespace JWT_User_Log_Reg_Auth.Controllers
 {
@@ -10,10 +11,24 @@ namespace JWT_User_Log_Reg_Auth.Controllers
     {
         public static UserModel user = new UserModel();
 
-        [HttpPost("reguster")]
+        [HttpPost("register")]
         public async Task<ActionResult<UserModel>>Register(UserDtoModel request)
         {
-            return Ok();
+            CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
+            user.Username = request.Username;
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            return Ok(user);
+        }
+
+        private void CreatePasswordHash(string password,out byte[] passwordHash,out byte[] passwordSalt)
+        {
+            using(var hmac=new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
         }
     }
 }
